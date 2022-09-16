41d0ea8c5f81fac13cbc3090b581f109a869b72f
TAKAYAMA Hiroshi <lamartakayama@gmail.com>
2022-09-16 17:51:33 +0900

    小説テンプレートページの追加

--- taxonomy-genre-novel.php
+++ taxonomy-genre-novel.php
@@ -1,1 +1,59 @@
-小説用のテンプレートです
+<?php
+/**
+ * The template for displaying archive pages
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ *
+ * @package hiroshi
+ */
+
+get_header();
+?>
+
+	<main id="primary" class="site-main col-md-8">
+
+		<?php if ( have_posts() ) : ?>
+
+			<header class="page-header">
+				<h1>タカヤマ備忘録</h1>
+
+			<?php $terms = get_terms('genre'); ?>
+			<ul class="nav">
+				<?php foreach ($terms as $term): ?>
+					<li class="nav-item"><a class="nav-link" href="<?php echo get_term_link($term); ?>"><?php echo esc_html($term->name); ?></a></li>
+				<?php endforeach; ?>
+			</ul>
+
+                <h2><?php the_archive_title(); ?></h2>
+			</header><!-- .page-header -->
+
+		<ul>
+			<?php
+			/* Start the Loop */
+			while ( have_posts() ) :
+				the_post();	
+
+			?>
+
+			<li><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></li>
+
+		<?php
+			endwhile;
+		?>
+		</ul>
+
+		<?php
+			the_posts_navigation();
+
+			else :
+
+			get_template_part( 'template-parts/content', 'none' );
+
+		endif;
+		?>
+
+	</main><!-- #main -->
+
+<?php
+get_sidebar();
+get_footer();
