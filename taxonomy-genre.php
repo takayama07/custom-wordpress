eb747cffda2082719c85a37e558a012860f7470d
TAKAYAMA Hiroshi <lamartakayama@gmail.com>
2022-09-19 22:23:49 +0900

    カスタム分類のナビゲーションを作成

--- taxonomy-genre.php
+++ taxonomy-genre.php
@@ -16,6 +16,14 @@ get_header();
 
 			<header class="page-header">
 				<h1>タカヤマ備忘録</h1>
+
+			<?php $terms = get_terms('genre'); ?>
+			<ul class="nav">
+				<?php foreach ($terms as $term): ?>
+					<li class="nav-item"><a class="nav-link" href="<?php echo get_term_link($term); ?>"><?php echo esc_html($term->name); ?></a></li>
+				<?php endforeach; ?>
+			</ul>
+
                 <h2><?php the_archive_title(); ?></h2>
 			</header><!-- .page-header -->
 
