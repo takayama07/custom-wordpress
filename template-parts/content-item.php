2af02f81e10e28ee24d43462dfe45b5bcbe68f94
TAKAYAMA Hiroshi <lamartakayama@gmail.com>
2022-09-19 22:16:22 +0900

    価格と出版社の内容をカスタムフィールドで表示できるようにする

--- template-parts/content-item.php
+++ template-parts/content-item.php
@@ -1,1 +1,17 @@
 <p>商品名: <?php the_title(); ?></p>
+
+<?php $price = get_post_meta(get_the_ID(),'価格', true); ?>
+<?php $publisher = get_post_meta(get_the_ID(), '出版社', true); ?>
+
+<dl class="table">
+    <!-- priceの中身が空でなければ繰り返し出力する -->
+    <?php if ($price !== ''): ?>
+        <dt>価格</dt>
+        <dd><?php echo esc_html(number_format($price)); ?>円</dd>
+    <?php endif; ?>
+    <!-- publisherの中身が空でなければ繰り返し出力する -->
+    <?php if ($publisher !== ''): ?>
+        <dt>出版社</dt>
+        <dd><?php echo esc_html($publisher); ?></dd>
+    <?php endif; ?>
+</dl>
