55671292cc1aad30bf50ce458df8eedf6c19e04c
TAKAYAMA Hiroshi <lamartakayama@gmail.com>
2022-09-20 01:06:16 +0900

    カスタムフィールドを配列で取得

--- template-parts/content-item.php
+++ template-parts/content-item.php
@@ -2,7 +2,7 @@
 
 <?php $price = get_post_meta(get_the_ID(),'価格', true); ?>
 <?php $publisher = get_post_meta(get_the_ID(), '出版社', true); ?>
-
+<?php $writer = get_post_meta(get_the_ID(), '著者', false); ?>
 <dl class="table">
     <!-- priceの中身が空でなければ繰り返し出力する -->
     <?php if ($price !== ''): ?>
@@ -13,5 +13,11 @@
     <?php if ($publisher !== ''): ?>
         <dt>出版社</dt>
         <dd><?php echo esc_html($publisher); ?></dd>
+    <?php endif; ?>
+    <?php if ($writer): ?>
+        <dt>著者</dt>
+        <?php foreach ($writer as $w): ?>
+            <dd><?php echo esc_html($w); ?></dd>
+        <?php endforeach; ?>
     <?php endif; ?>
 </dl>
